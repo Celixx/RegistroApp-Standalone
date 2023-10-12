f1cfa80e6fc44548df35f184f09f40c4c2daafce
Celix <alonsomurua@gmail.com>
2023-10-11 21:26:55 -0300

    commit inicial con paginas, servicios y componentes inicializaos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,24 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'correcto',
+    loadComponent: () => import('./pages/correcto/correcto.page').then( m => m.CorrectoPage)
+  },
+  {
+    path: 'incorrecto',
+    loadComponent: () => import('./pages/incorrecto/incorrecto.page').then( m => m.IncorrectoPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'pregunta',
+    loadComponent: () => import('./pages/pregunta/pregunta.page').then( m => m.PreguntaPage)
+  },
 ];
